7bd7c0a08eedf4f8da268d39e2f835e2834a8361
Brett Jones <brettjonesdev@gmail.com>
2013-02-21 16:08:05 -0600

    Simplifying

--- public/js/app/models/Model.js
+++ public/js/app/models/Model.js
@@ -1,28 +1,22 @@
 define(["jquery", "backbone"],
-    function($, Backbone) {
+    function ($, Backbone) {
         // Creates a new Backbone Model class object
         var Model = Backbone.Model.extend({
-
-            // Model Constructor
-            initialize: function() {
+            initialize:function () {
 
             },
 
             // Default values for all of the Model attributes
-            defaults: {
+            defaults:{
 
             },
 
             // Get's called automatically by Backbone when the set and/or save methods are called (Add your own logic)
-            validate: function(attrs) {
+            validate:function (attrs) {
 
             }
-
         });
 
-        // Returns the Model class
         return Model;
-
     }
-
 );
